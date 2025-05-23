87b29ca5d42ddcfc2ad05d957ad62e71d12a0e2d
Senior Developer <eng.tiagorocha@gmail.com>
2025-05-23 15:04:47 -0300

    initial commit application

---
+++ start/routes.ts
@@ -0,0 +1,28 @@
+/*
+|--------------------------------------------------------------------------
+| Routes file
+|--------------------------------------------------------------------------
+|
+| The routes file is used for defining the HTTP routes.
+|
+*/
+
+import router from '@adonisjs/core/services/router'
+// const InstitutionsController = () => import('#controllers/institutions_controller')
+// const ClassesController = () => import('#controllers/classes_controller')
+// const StudentsController = () => import('#controllers/students_controller')
+// const RegistrationsController = () => import('#controllers/registrations_controller')
+
+router
+  .group(() => {
+    router.get('/', async () => {
+      return {
+        hello: 'world',
+      }
+    })
+    // router.resource('/institutions', InstitutionsController).apiOnly()
+    // router.resource('/classes', InstitutionsController).apiOnly()
+    // router.resource('/students', InstitutionsController).apiOnly()
+    // router.resource('/registrations', InstitutionsController).apiOnly()
+  })
+  .prefix('/api')
